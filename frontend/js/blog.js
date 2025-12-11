9fd37274b85fcfef337fab63a440969da0b31c2f
david rugeles <david.rugeles@pi.edu.co>
2025-12-11 10:41:03 -0500

    mi ultima clase

--- frontend/js/blog.js
+++ frontend/js/blog.js
@@ -1,7 +1,20 @@
 const API_URL = "http://localhost:3000/api/recetas/"
 
-fetch(API_URL).then(response => response.json()).then(data =>{
+fetch(API_URL)
+.then(response => response.json())
+.then(datos =>{
 
+        let seccion = document.getElementById("recetas-lista");
+        datos.daticos.forEach(receta => {
+              seccion.innerHTML += `
+        <div class="receta-card">
+                <img src="${receta.imagen_url}" alt="">
+                <h3>${receta.titulo}</h3>
+                <p class="receta-time">⏱️${receta.tiempo_preparacion}</p>
+                <p<
+             </div> 
+        `  
+        });
         
 
 })
